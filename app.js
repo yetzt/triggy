1e503421878cf3fa72a07ddeb45d0c877084be87
Sebastian Vollnhals <sebastian@vollnhals.info>
2012-04-03 00:23:43 +0200

    added some comments so i don't have to be ashamed

--- app.js
+++ app.js
@@ -9,30 +9,40 @@ var triggy = require("./lib/triggy");
 var prefix = 'http://tr.gy/';
 
 process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ' + err);
+	/* nodejs version of "on error resume next" */
+	console.log('Caught exception: ' + err);
 });
 
+/* add some locales for i18n fun */
 i18n.configure({
     locales:['en', 'de'],
     register: global
 });
 
+/* create a server with express */
 var app = express.createServer();
 
-app.configure(function(){
+app.configure(function() {
+	/* we use embedded js as kind of template engine */
 	app.set('view engine', 'ejs');
+	/* since we get posted we want a bodyParser() */
 	app.use(express.bodyParser());
+	/* express-validator makes your input handling safety way easier */
 	app.use(exval);
+	/* can we haz i18n? */
 	app.use(i18n.init);
+	/* just serve the assets till we find out how to handle that stuff with nginx */
 	app.use(express.static(__dirname+'/assets'));
+	/* we use router to server differen paths */
 	app.use(app.router);
 });
 
+/* serve the index document when nothing special is requested */
 app.get('/', function(req, res){
 	res.render('index');
 });
 
-
+/* serve the trigger warning if an existing short url is called */
 app.get(/^\/([\_A-Za-z0-9]{5})$/, function(req, res){
 	triggy.get(req.params[0], function(result){
 		if (result === null) {
@@ -43,6 +53,7 @@ app.get(/^\/([\_A-Za-z0-9]{5})$/, function(req, res){
 	});
 });
 
+/* json interface for short link creation */
 app.post('/api/create', function(req, res){
 	
 	var errors = [];
@@ -70,6 +81,7 @@ app.post('/api/create', function(req, res){
 
 });
 
+/* html interface for short link creation, for those without javascript */
 app.post('/create', function(req, res){
 
 	var errors = [];
@@ -95,8 +107,10 @@ app.post('/create', function(req, res){
 
 });
 
+/* just redirect anything else to the default page */
 app.get('*', function(req, res){
 	res.redirect('/', 301);
 });
 
+/* run on localhost port 9998 */
 app.listen(9998,"127.0.0.1");
